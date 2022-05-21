c4127996ad045378be23794c5376149d21b6c416
Aulia <105323573+N3WBI31@users.noreply.github.com>
2022-05-21 23:15:33 +0700

    Add files via upload

---
+++ Sad.sh
@@ -0,0 +1,248 @@
+#Coded by RelGanz
+#Jangan di recode bangsat!!
+#Mau recode izin ke nomor 0882-1098-2908
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+clear
+echo $indigo"==================================================================="
+                    toilet -f future Sadboy Sadgirl | lolcat
+                    figlet -f future Kumpulan kata-kata Sad | lolcat
+echo $indigo"*"     $yellow"Author  : RelGanz"
+echo $indigo"*"     $yellow"Support : Aulia"
+echo $indigo"*"     $yellow"Team    : Bekasi Cyber Team"
+echo $indigo"*"     $yellow"Contact : 0882-1098-2908
+                                    : 0858-8061-9463"
+echo $indigo"==================================================================="
+echo
+echo $cyan "1. Sadboy"
+echo $cyan "2. Sadboy"
+echo $green "3. Sadboy"
+echo $green "4. Sadboy"
+echo $indigo "5. Sadboy"
+echo $indigi " 6. Sadgirl"
+echo $red "7. Sadgirl"
+echo $red "8. Sadgirl"
+echo $yellow "9. Sadgirl"
+echo $yellow "10. Sadgirl"
+echo
+echo $indigo"╭─Pilih Mau kata-kata Sad Yg Mana?? "
+read -p "╰─•>  " pil
+
+if [ $pil = '1' ]
+then
+echo
+echo
+clear
+echo
+toilet -f future Sad Bro:) | lolcat
+echo
+echo
+echo
+sleep 0.1
+echo $green"Sadboy"
+sleep 2
+echo $green"Bagi mu aku hanya tempat sesaat untuk menghindari hujan jika pelangi mu datang kau pasti pergi "
+sleep 3
+clear
+echo
+figlet -f future Sad Bro:) | lolcat
+echo
+echo
+echo
+sleep 0.1
+echo $red"Baper?"
+echo
+echo
+sleep 2
+echo $cyan"Jadi orang kok baperan banget??"
+sleep 2
+echo $red"ya iyalah.. namanya juga manusia!"
+sleep 2
+echo $cyan"punya otak, punya hati."
+sleep 1
+echo $red"beda sama lu yg ngomongnya suka ceplas-ceplos!"
+sleep 2
+echo $cyan"hati mati, otak gak berfungsi"
+sleep 2
+echo $red"haha.."
+sleep 2
+echo $cyan"ya.. lain kali bisalah.."
+sleep 2
+echo $red"ngomong. tapi mikir.. :)"
+sleep 3
+fi
+
+if [ $pil = '2' ]
+then
+echo
+echo
+clear
+echo
+toilet -f future Sad Bro:) | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Sadboy"
+sleep 2
+echo $red"Jika menurut mu dia yg terbaik pergi lah aku tidak ingin menjadi penghalang ke bahagiaan mu"
+sleep 3
+fi
+
+if [ $pil = '3' ]
+then
+clear
+echo
+toilet -f future Sad Bro:) | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Biarpun kamu disana bersamanya.."
+sleep 2
+echo $green"aku tetap menunggumu sampai.."
+sleep 2
+echo $green"kau bisa memahami perasaanku.. :')"
+sleep 2
+echo $green"yang hampa ini kepadamu.."
+sleep 2
+echo $green"biarpun sayang tetapi kita...."
+echo
+echo
+sleep 3
+figlet -f future Cuma Temenan | lolcat
+sleep 3
+fi
+
+if [ $pil = '4' ]
+then
+clear
+echo
+toilet -f future Sad Bro:) | lolcat
+echo
+echo
+sleep 0.1
+echo $indigo"Bukan kamu yg terlalu sibuk cuma aku aja yg berharap lebih ke kamu"
+echo
+echo
+sleep 10
+echo $red"ku tidak pingin kamu pergi meninggalkan ku sendiri berbarengan bayangmu memanglah kita tlah jauh rasanya memanglah kita telah tidak bersama bila memanglah kita ditakdirkan tuk berbarengan selamanya cinta takkan kemana.."
+echo
+echo
+sleep 10
+echo $indigo"Aku mungkin sering membuatmu kesal, tapi percayalah aku tak akan membuatmu menyesal telah bersama ku , Good Morning"
+echo
+echo
+sleep 10
+echo $red"Setiap pertemuan pasti ada perpisahan,namun entah kenapa perpisahan selalu saja terasa berat,apalagi untuk seseorang yang selalu ada dalam hidup ini.."
+sleep 3
+fi
+
+if [ $pil = '5' ]
+then
+clear
+echo
+toilet -f future Sad Bro:) | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Terkadang aku bodoh seperti nobita Memperjuangkan seseorang yang dia tidak  tahu hatinya untuk siapa"
+sleep 3
+fi
+
+if [ $pil = '6' ]
+then
+clear
+echo
+toilet -f future Sad Mba:) | lolcat
+echo
+echo
+sleep 0.1
+echo $white"Aku tau sebenarnya aku bukan yg sempurna di mata mu tpi percaya lah kau adalah org yg paling sepesial yg pernah ku temui"
+sleep 3
+fi
+
+if [ $pil = '7' ]
+then
+clear
+echo
+toilet -f future Sad Mba:) | lolcat
+echo
+echo
+sleep 0.1
+echo $red"Aku tau sebenarnya aku bukan yg sempurna di mata mu tpi percaya lah kau adalah org yg paling sepesial yg pernah ku temui"
+sleep 3
+fi
+
+if [ $pil = '8' ]
+then
+clear
+echo
+toilet -f future Sad Mba:) | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Sad By Aulia"
+sleep 2
+echo $yellow"Sadgirl
+Aku hanya meminta waktu mu bukan dunia mu sdrlah aku hnya gadis kecil yg ingin di berikan perhatian"
+sleep 3
+clear
+echo
+toilet -f future Sad By Aulia | lolcat
+echo
+echo
+sleep 0.1
+echo $indigo"aku sudah menjadi kan mu rumah tpi kau hanya menjadi kan ku tempat singgah"
+sleep 
+clear
+echo
+toilet -f future Sad By Aulia | lolcat
+echo
+echo
+sleep 0.1
+echo $red"Terima kasih pernah singgah walaupun singkat tpi kenangan ini berharga bagi ku"
+sleep 2
+fi
+
+if [ $pil = '9' ]
+then
+clear
+echo
+toilet -f future Sad Mba:) | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Tidak ada yang setia di dunia ini bahkan bayanganmu sendiripun akan pergi menghilang dalam kegelapan"
+sleep 2
+echo
+echo $indigo"Apa kah aku harus mengganti namaku menjadi aamiin,agar selalu kau sebut dalam doa mu:)"
+sleep 2
+echo
+echo $green"Bukan kebahagian yg membuat kita bersyukur,tetapi bersyukurlah yg membuat kita bahagia:)"
+sleep 3
+fi
+
+if [ $pil = '10' ]
+then
+clear
+echo
+figlet -f future Sad Mab:) | lolcat
+echo
+echo
+sleep 0.1
+echo $green"Cinta itu dari hati yang terikat di dalam fikiran. Jika hati sudah saling bisa memahami dan meyakininya itu baru dinamakan cinta"
+echo
+sleep 2
+echo $red"Jika kamu mencintai dua orang dalam satu waktu pilihlah yang pertama karena jika kamu benar-benar  mencintai yang pertama tidak akan pernah ada yg kedua"
+sleep 3
+fi
+
+#thanks udh gunain toolsnya
+#belajar coding
+#recode izin 0882-1098-2908  
